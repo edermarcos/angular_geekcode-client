19968785cdbce9ee3e484c2df190ca1cd2dc4840
Jose Eder <skrap.marcos@gmail.com>
2019-01-18 16:19:24 -0600

    Added components

--- src/app/pages.module.ts
+++ src/app/pages.module.ts
@@ -6,12 +6,27 @@ import { HomeComponent } from './home/home.component';
 import { ComponentsModule } from './shared/components/components.module';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile/profile.component';
+import { CoursesComponent } from './courses/courses.component';
+import { FriendsComponent } from './friends/friends.component';
+import { NewsComponent } from './news/news.component';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'home',
     // canActivate: [ AuthGuardService ],
     component: HomeComponent
+  }, {
+    path: 'courses',
+    // canActivate: [ AuthGuardService ],
+    component: CoursesComponent
+  }, {
+    path: 'news',
+    // canActivate: [ AuthGuardService ],
+    component: NewsComponent
+  }, {
+    path: 'friends',
+    // canActivate: [ AuthGuardService ],
+    component: FriendsComponent
   }, {
     path: 'profile',
     // canActivate: [ AuthGuardService ],
@@ -19,7 +34,7 @@ const routes: Routes = [
   }, {
     path: '**',
     pathMatch: 'full',
-    redirectTo: ''
+    redirectTo: 'home'
   }
 ];
 
@@ -27,6 +42,9 @@ const routes: Routes = [
   declarations: [
     HomeComponent,
     ProfileComponent,
+    CoursesComponent,
+    FriendsComponent,
+    NewsComponent,
   ],
   imports: [
     RouterModule.forChild(routes),
@@ -36,6 +54,10 @@ const routes: Routes = [
   exports: [
     RouterModule,
     HomeComponent,
+    ProfileComponent,
+    CoursesComponent,
+    FriendsComponent,
+    NewsComponent,
   ]
 })
 export class PagesModule { }
